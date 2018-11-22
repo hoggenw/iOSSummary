888b3cee1b8d93b39f8785d32676a95b9632ed52
hoggenw <253192463@qq.com>
2018-11-22 10:16:49 +0800

    no message

---
+++ iOSProject/iOSProject/Code/Utils/FileManager.h
@@ -0,0 +1,13 @@
+//
+//  FileManager.h
+//  YLAudioFrequecy
+//
+//  Created by 王留根 on 2018/8/2.
+//  Copyright © 2018年 ios-mac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FileManager : NSObject
+
+@end
