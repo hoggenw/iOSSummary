1b9077e7cd04a3915b23d153cc61bfa4d48f3e5c
wanglg <253192463@qq.com>
2019-01-09 10:03:25 +0800

    直播

---
+++ iOSProject/iOSProject/Code/Module/Helper/LiveBroadcast/IJKFrameworkViewController.h
@@ -0,0 +1,13 @@
+//
+//  IJKFrameworkViewController.h
+//  iOSProject
+//
+//  Created by 王留根 on 2019/1/8.
+//  Copyright © 2019年 hoggenWang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IJKFrameworkViewController : UIViewController
+
+@end
---
+++ iOSProject/iOSProject/Code/Module/Helper/LiveBroadcast/IJKFrameworkViewController.m
@@ -0,0 +1,42 @@
+//
+//  IJKFrameworkViewController.m
+//  iOSProject
+//
+//  Created by 王留根 on 2019/1/8.
+//  Copyright © 2019年 hoggenWang.com. All rights reserved.
+//
+
+#import "IJKFrameworkViewController.h"
+
+@interface IJKFrameworkViewController ()
+
+@end
+
+@implementation IJKFrameworkViewController
+
+
+#pragma mark - Override Methods
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+#pragma mark - Public Methods
+
+
+#pragma mark - Events
+
+
+#pragma mark - Private Methods
+
+
+#pragma mark - Extension Delegate or Protocol
+
+@end
