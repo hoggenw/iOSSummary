5c33eb900e8a669603bff1d94313a84c98e7d520
wanglg <253192463@qq.com>
2019-01-12 11:02:13 +0800

    合并

---
+++ iOSProject/iOSProject/Code/Module/Base/BaiduMM.h
@@ -0,0 +1,17 @@
+//
+//  BaiduMM.h
+//  iOSProject
+//
+//  Created by 王留根 on 2019/1/12.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BaiduMM : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSProject/iOSProject/Code/Module/Base/BaiduMM.mm
@@ -0,0 +1,13 @@
+//
+//  BaiduMM.m
+//  iOSProject
+//
+//  Created by 王留根 on 2019/1/12.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import "BaiduMM.h"
+
+@implementation BaiduMM
+
+@end
