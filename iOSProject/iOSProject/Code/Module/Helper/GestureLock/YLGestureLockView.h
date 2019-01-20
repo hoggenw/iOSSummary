eacd682adb063b36fb445b37ba8a54759595c439
wanglg <253192463@qq.com>
2019-01-20 10:47:03 +0800

    no message

---
+++ iOSProject/iOSProject/Code/Module/Helper/GestureLock/YLGestureLockView.h
@@ -0,0 +1,17 @@
+//
+//  YLGestureLockView.h
+//  iOSProject
+//
+//  Created by 王留根 on 2019/1/20.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YLGestureLockView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSProject/iOSProject/Code/Module/Helper/GestureLock/YLGestureLockView.m
@@ -0,0 +1,21 @@
+//
+//  YLGestureLockView.m
+//  iOSProject
+//
+//  Created by 王留根 on 2019/1/20.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import "YLGestureLockView.h"
+
+@implementation YLGestureLockView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
