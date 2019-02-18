b2f40c459da6b4fef53ba7617fcd377f7c41f997
wanglg <253192463@qq.com>
2019-02-18 14:49:19 +0800

    准备功能点页面

---
+++ iOSProject/iOSProject/Code/Module/Page2/Controllers/Page2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Page2ViewController.h
+//  iOSProject
+//
+//  Created by 王留根 on 2019/2/18.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Page2ViewController : UIViewController
+
+@end
---
+++ iOSProject/iOSProject/Code/Module/Page2/Controllers/Page2ViewController.m
@@ -0,0 +1,42 @@
+//
+//  Page2ViewController.m
+//  iOSProject
+//
+//  Created by 王留根 on 2019/2/18.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import "Page2ViewController.h"
+
+@interface Page2ViewController ()
+
+@end
+
+@implementation Page2ViewController
+
+
+#pragma mark - Override Methods
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+#pragma mark - Public Methods
+
+
+#pragma mark - Events
+
+
+#pragma mark - Private Methods
+
+
+#pragma mark - Extension Delegate or Protocol
+
+@end
