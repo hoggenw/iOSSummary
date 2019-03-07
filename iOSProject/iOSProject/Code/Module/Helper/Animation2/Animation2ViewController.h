0b0ed341f2ec445a6a68ca3ed5c0ba8cbcf282ba
wanglg <253192463@qq.com>
2019-03-07 15:16:55 +0800

    no message

---
+++ iOSProject/iOSProject/Code/Module/Helper/Animation2/Animation2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Animation2ViewController.h
+//  iOSProject
+//
+//  Created by 王留根 on 2019/3/7.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Animation2ViewController : UIViewController
+
+@end
---
+++ iOSProject/iOSProject/Code/Module/Helper/Animation2/Animation2ViewController.m
@@ -0,0 +1,42 @@
+//
+//  Animation2ViewController.m
+//  iOSProject
+//
+//  Created by 王留根 on 2019/3/7.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import "Animation2ViewController.h"
+
+@interface Animation2ViewController ()
+
+@end
+
+@implementation Animation2ViewController
+
+
+#pragma mark - Override Methods
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+#pragma mark - Public Methods
+
+
+#pragma mark - Events
+
+
+#pragma mark - Private Methods
+
+
+#pragma mark - Extension Delegate or Protocol
+
+@end
