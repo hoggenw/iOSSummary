3d16891c35877f5716fcca10ebae9f32877c5d18
wanglg <253192463@qq.com>
2019-03-14 14:30:50 +0800

    no message

---
+++ iOSProject/iOSProject/Code/Custom/YLGradientWKWebview.h
@@ -0,0 +1,17 @@
+//
+//  YLGradientWKWebview.h
+//  iOSProject
+//
+//  Created by 王留根 on 2019/3/14.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import <WebKit/WebKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YLGradientWKWebview : WKWebView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSProject/iOSProject/Code/Custom/YLGradientWKWebview.m
@@ -0,0 +1,21 @@
+//
+//  YLGradientWKWebview.m
+//  iOSProject
+//
+//  Created by 王留根 on 2019/3/14.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import "YLGradientWKWebview.h"
+
+@implementation YLGradientWKWebview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
