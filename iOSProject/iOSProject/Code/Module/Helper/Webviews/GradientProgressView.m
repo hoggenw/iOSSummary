f434d95e3171a66c30ea20a9f507c51b81886607
wanglg <253192463@qq.com>
2019-03-15 10:40:35 +0800

    no message

---
+++ iOSProject/iOSProject/Code/Module/Helper/Webviews/GradientProgressView.m
@@ -0,0 +1,21 @@
+//
+//  GradientProgressView.m
+//  iOSProject
+//
+//  Created by 王留根 on 2019/3/14.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import "GradientProgressView.h"
+
+@implementation GradientProgressView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
