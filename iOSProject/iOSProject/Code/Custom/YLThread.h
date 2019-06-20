4b4d303ad4138799623923a3863b4bdfd57a827c
hoggenw <253192463@qq.com>
2019-06-20 17:04:23 +0800

    no message

---
+++ iOSProject/iOSProject/Code/Custom/YLThread.h
@@ -0,0 +1,17 @@
+//
+//  YLThread.h
+//  iOSProject
+//
+//  Created by 王留根 on 2019/6/19.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YLThread : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSProject/iOSProject/Code/Custom/YLThread.m
@@ -0,0 +1,13 @@
+//
+//  YLThread.m
+//  iOSProject
+//
+//  Created by 王留根 on 2019/6/19.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import "YLThread.h"
+
+@implementation YLThread
+
+@end
