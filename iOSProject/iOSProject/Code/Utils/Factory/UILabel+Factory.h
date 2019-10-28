1d1b14601e44abd59d126db25aeb7d0854c3e970
hoggenw <hoggenwang@qq.com>
2019-10-28 15:25:52 +0800

    UILabel 工厂类

---
+++ iOSProject/iOSProject/Code/Utils/Factory/UILabel+Factory.h
@@ -0,0 +1,20 @@
+//
+//  UILabel+Factory.h
+//  iOSProject
+//
+//  Created by Alexander on 2019/10/28.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UILabel (Factory)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOSProject/iOSProject/Code/Utils/Factory/UILabel+Factory.m
@@ -0,0 +1,16 @@
+//
+//  UILabel+Factory.m
+//  iOSProject
+//
+//  Created by Alexander on 2019/10/28.
+//  Copyright © 2019 hoggenWang.com. All rights reserved.
+//
+
+#import "UILabel+Factory.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation UILabel (Factory)
+
+@end
