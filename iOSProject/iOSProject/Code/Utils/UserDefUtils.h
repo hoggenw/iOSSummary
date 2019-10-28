8204daaee2e10f4c11940b18db6979c80a8931fa
hoggenw <hoggenwang@qq.com>
2019-10-28 11:28:34 +0800

    添加Plist文件数据存储

--- iOSProject/iOSProject/Code/Utils/UserDefUtils.h
+++ iOSProject/iOSProject/Code/Utils/UserDefUtils.h
@@ -36,4 +36,35 @@
  *  字符串本地取出
  */
 + (NSString *) getStringForKey:(NSString *)key;
+
+
+
+#pragma mark - Plist文件数据存储
+
+/**
+ Plist数据保存
+ 
+ @param plistName Plist文件名称
+ @param dataObj 对象数据（NSMutableDictionary、NSArray）
+ @return YES:成功 NO:失败
+ */
++ (BOOL)savePlist:(NSString *)plistName dataObj:(NSObject *)dataObj;
+
+/**
+ Plist数据读取
+ 
+ @param plistName Plist文件名称
+ @param dataClass 数据类型
+ @return 对象数据
+ */
++ (nullable id)readPlist:(NSString *)plistName dataClass:(Class)dataClass;
+
+/**
+ Plist数据删除
+ 
+ @param plistName Plist文件名称
+ @return YES:成功 NO:失败
+ */
++ (BOOL)removePlist:(NSString *)plistName;
+
 @end
--- iOSProject/iOSProject/Code/Utils/UserDefUtils.m
+++ iOSProject/iOSProject/Code/Utils/UserDefUtils.m
@@ -72,4 +72,65 @@
     return string;
 }
 
+
+#pragma mark - Plist文件数据存储
+
+/**
+ Plist数据保存
+ 
+ @param plistName Plist文件名称
+ @param dataObj 对象数据（NSMutableDictionary、NSArray）
+ @return YES:成功 NO:失败
+ */
++ (BOOL)savePlist:(NSString *)plistName dataObj:(NSObject *)dataObj {
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *plistPath = [path stringByAppendingPathComponent:plistName];
+    if ([dataObj isKindOfClass:[NSMutableDictionary class]]) {
+        return [((NSMutableDictionary *)dataObj) writeToFile:plistPath atomically:YES];
+    }
+    else if ([dataObj isKindOfClass:[NSArray class]]) {
+        return [((NSArray *)dataObj) writeToFile:plistPath atomically:YES];
+    }
+    else {
+        return NO;
+    }
+}
+
+/**
+ Plist数据读取
+ 
+ @param plistName Plist文件名称
+ @param dataClass 数据类型
+ @return 对象数据
+ */
++ (nullable id)readPlist:(NSString *)plistName dataClass:(Class)dataClass {
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *plistPath = [path stringByAppendingPathComponent:plistName];
+    if ([dataClass isSubclassOfClass:[NSMutableDictionary class]]) {
+        return [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
+    }
+    else if ([dataClass isSubclassOfClass:[NSArray class]]) {
+        return [NSMutableArray arrayWithContentsOfFile:plistPath];
+    }
+    else {
+        return nil;
+    }
+}
+
+/**
+ Plist数据删除
+ 
+ @param plistName Plist文件名称
+ @return YES:成功 NO:失败
+ */
++ (BOOL)removePlist:(NSString *)plistName {
+    NSString *path = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+    NSString *plistPath = [path stringByAppendingPathComponent:plistName];
+    NSFileManager *fileManager = [[NSFileManager alloc] init];
+    if ([fileManager fileExistsAtPath:plistPath]) {
+        [fileManager removeItemAtPath:plistPath error:nil];
+    }
+    return YES;
+}
+
 @end
